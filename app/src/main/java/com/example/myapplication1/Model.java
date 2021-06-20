79dd375265c5708fc9c3b3524a545ae426af15a0
Ella Kharakh <ella.kharakh@live.biu.ac.il>
2021-06-20 23:48:20 +0300

    MODEL AND VIEWMODEL ARE READY TO GO

--- app/src/main/java/com/example/myapplication1/Model.java
+++ app/src/main/java/com/example/myapplication1/Model.java
@@ -1,4 +1,100 @@
 package com.example.myapplication1;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.LinkedBlockingQueue;
+
 public class Model {
+    private BlockingQueue<Runnable> taskQueue = new LinkedBlockingQueue<Runnable>();
+    private PrintWriter printIt;
+    private boolean terminate = false;
+
+    public Model() {
+        new Thread(new Runnable() {
+            public void run() {
+                while (!terminate) {
+                    try {
+                        if(taskQueue.isEmpty()){
+                            closeTheThread();
+                        }
+                        taskQueue.take().run();
+                    } catch (InterruptedException e) {}
+                }
+            }
+        }).start();
+    }
+
+    public void closeTheThread() throws InterruptedException {
+        taskQueue.put(new Runnable() {
+            public void run() {
+                terminate = true;
+            }
+        });
+    }
+
+    void setAileron(double val) throws InterruptedException {
+        if(printIt!=null){
+            taskQueue.put(new Runnable() {
+                public void run() {
+                    printIt.print("set /controls/flight/aileron "+val+"\r\n");
+                    printIt.flush();
+                }
+            });
+        }
+
+    }
+
+    void setThrottle(double val) throws InterruptedException {
+        if(printIt!=null){
+            taskQueue.put(new Runnable() {
+                public void run() {
+                    printIt.print("set /controls/engines/current-engine/throttle "+val+"\r\n");
+                    printIt.flush();
+                }
+            });
+        }
+
+    }
+
+    void setRudder(double val) throws InterruptedException {
+        if(printIt!=null){
+            taskQueue.put(new Runnable() {
+                public void run() {
+                    printIt.print("set /controls/flight/rudder "+val+"\r\n");
+                    printIt.flush();
+                }
+            });
+        }
+
+    }
+
+    void setElevator(double val) throws InterruptedException {
+        if(printIt!=null){
+            taskQueue.put(new Runnable() {
+                public void run() {
+                    printIt.print("set /controls/flight/elevator "+val+"\r\n");
+                    printIt.flush();
+                }
+            });
+        }
+    }
+    void connectToSimulatorInModel(String ip, int port) throws InterruptedException {
+        taskQueue.put(new Runnable() {
+            public void run() {
+                Socket fg = null;
+                try {
+                    fg = new Socket(ip, port);
+                    printIt = new PrintWriter(fg.getOutputStream(), true);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        });
+    }
+
 }
--- app/src/main/java/com/example/myapplication1/ViewModel.java
+++ app/src/main/java/com/example/myapplication1/ViewModel.java
@@ -1,4 +1,30 @@
 package com.example.myapplication1;
 
 public class ViewModel {
+    private Model mdl;
+
+    public ViewModel(Model m){
+        this.mdl = m;
+    }
+
+    public void setAileronForViewModel(double val) throws InterruptedException {
+
+        this.mdl.setAileron(val);
+    }
+
+    public void setElevatorForViewModel(double val) throws InterruptedException {
+
+        this.mdl.setElevator(val);
+    }
+
+    public void setThrottleForViewModel(double val) throws InterruptedException {
+        this.mdl.setThrottle(val);
+    }
+    public void setRudderForViewModel(double val) throws InterruptedException {
+        this.mdl.setRudder(val);
+    }
+
+    public void connectToSimulator(String ip, int port) throws InterruptedException {
+        this.mdl.connectToSimulatorInModel(ip, port);
+    }
 }
