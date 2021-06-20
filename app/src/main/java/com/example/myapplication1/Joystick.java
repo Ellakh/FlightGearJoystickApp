b7ae4b90a3c0084604224716d07f2671b435db25
Ella Kharakh <ella.kharakh@live.biu.ac.il>
2021-06-20 03:50:05 +0300

    great project

---
+++ app/src/main/java/com/example/myapplication1/Joystick.java
@@ -0,0 +1,10 @@
+package com.example.myapplication1;
+
+import android.content.Context;
+import android.view.View;
+
+public class Joystick extends View {
+    public Joystick(Context context) {
+        super(context);
+    }
+}
